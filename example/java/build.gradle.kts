1496d3943cb140e4aeaae8acaa1e6bdcc09bbfdd
Phillip Schichtel <phillip@schich.tel>
2024-08-05 00:35:24 +0200

    attempt building native images
    currently it doesn't produce anything, but at least the toolchain seems ok.

--- example/java/build.gradle.kts
+++ example/java/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 repositories {
@@ -20,4 +21,25 @@ java {
 
 application {
     mainClass = "tel.schich.libdatachannel.example.Main"
+}
+
+val graalToolchain = javaToolchains.launcherFor {
+    languageVersion = JavaLanguageVersion.of(21)
+    vendor = JvmVendorSpec.GRAAL_VM
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            resources.autodetect()
+            imageName = project.name
+            mainClass = application.mainClass.get()
+            buildArgs.add("-O4")
+            useFatJar = true
+        }
+    }
+    binaries.all {
+        buildArgs.add("--verbose")
+        javaLauncher = graalToolchain
+    }
 }
--- example/java/settings.gradle.kts
+++ example/java/settings.gradle.kts
@@ -1,3 +1,16 @@
 rootProject.name = "libdatachannel-java-example"
 
 includeBuild("../..")
+
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://raw.githubusercontent.com/graalvm/native-build-tools/snapshots")
+        }
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
