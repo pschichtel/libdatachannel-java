6162ba3450e4394184c980313677ab0bc2536d80
Phillip Schichtel <phillip@schich.tel>
2025-06-05 01:34:41 +0200

    add a thread destructor function to detach threads again
    this is what's suggested by android: https://developer.android.com/training/articles/perf-jni#threads

--- jni/src/init.c
+++ jni/src/init.c
@@ -1,24 +1,40 @@
 #include <rtc/rtc.h>
 #include <jni.h>
+#include <pthread.h>
 #include "global_jvm.h"
 #include "jni-c-to-java.h"
 
 static JavaVM* global_JVM;
+static pthread_key_t thread_key;
+
+void detach_thread() {
+    JavaVM* jvm = pthread_getspecific(thread_key);
+    if (jvm != NULL) {
+        (*jvm)->DetachCurrentThread(jvm);
+    }
+}
+
+JNIEnv* get_jni_env_from_jvm(JavaVM* jvm) {
+    JNIEnv* env;
+    jint result = (*jvm)->GetEnv(jvm, (void **)&env, JNI_VERSION_10);
+    if (result == JNI_EDETACHED) {
+        result = (*jvm)->AttachCurrentThreadAsDaemon(jvm, (void**)&env, NULL);
+        if (result == JNI_OK) {
+            pthread_setspecific(thread_key, jvm);
+        }
+    }
+    if (result != JNI_OK) {
+        return NULL;
+    }
+    return env;
+}
 
 JNIEnv* get_jni_env() {
     // make sure it's initialized
     if (global_JVM == NULL) {
         return NULL;
     }
-    JNIEnv* env;
-    jint result = (*global_JVM)->GetEnv(global_JVM, (void **)&env, JNI_VERSION_10);
-    if (result == JNI_EDETACHED) {
-        result = (*global_JVM)->AttachCurrentThreadAsDaemon(global_JVM, (void**)&env, NULL);
-    }
-    if (result != JNI_OK) {
-        return NULL;
-    }
-    return env;
+    return get_jni_env_from_jvm(global_JVM);
 }
 
 void logger_callback(rtcLogLevel level, const char *message) {
@@ -33,6 +49,7 @@ void logger_callback(rtcLogLevel level, const char *message) {
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
 {
+    pthread_key_create(&thread_key, detach_thread);
     global_JVM = jvm;
     rtcInitLogger(RTC_LOG_VERBOSE, &logger_callback);
     rtcPreload();
