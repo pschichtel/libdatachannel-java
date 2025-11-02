76aa1eb1897597f07c76ac35e0edcc19867b3686
Phillip Schichtel <phillip@schich.tel>
2025-11-02 18:56:23 +0100

    minor warning fix

--- arch-detect/src/main/java/tel/schich/libdatachannel/LibDataChannelArchDetect.java
+++ arch-detect/src/main/java/tel/schich/libdatachannel/LibDataChannelArchDetect.java
@@ -1,6 +1,9 @@
 package tel.schich.libdatachannel;
 
-public class LibDataChannelArchDetect {
+public abstract class LibDataChannelArchDetect {
+
+    private LibDataChannelArchDetect() {}
+
     public static void initialize() {
         System.setProperty(Platform.classPathPropertyNameForLibrary(LibDataChannel.LIB_NAME), "/" + Platform.detectArch() + "/native/" + Platform.libraryFilename(LibDataChannel.LIB_NAME));
         LibDataChannel.initialize();
